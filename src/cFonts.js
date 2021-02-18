24a02939856d61db05ee1606cd78cc7f9edea673
Johnson <zxmagichacker@gmail.com>
2021-02-18 17:55:07 +0800

    feat: 处理定制化logo和slogan

---
+++ src/cFonts.js
@@ -0,0 +1,14 @@
+/**
+ * 输出定制化logo和slogan
+ */
+const cFonts = require('cfonts')
+cFonts.say('code lint', {
+  font: 'block',
+  align: 'left',
+  colors: ['#f80'],
+  background: 'transparent',
+  letterSpacing: 0,
+  lineHeight: 1,
+  space: true,
+  maxLength: '0',
+});
