8a181108f1d10b2447b6a0e1daff67eadf84b6f7
Johnson <zxmagichacker@gmail.com>
2021-02-19 00:18:03 +0800

    feat: 重构入口文件

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,20 @@
 #!/usr/bin/env node
 require('../src/cFonts')
-require('../src/command')
+const { program } = require('commander')
+const { name, version } = require('../src/utils/constants')
+
+program
+  // 定义当前版本
+  .version(version, '-v, --version', 'output the version number')
+  // 定义用法
+  .usage('<command> [options]')
+
+program
+  // 定义初始化指令
+  .command('init')
+  .description('init relative repository')
+  .action(() => {
+    console.log('init')
+  })
+// 解析参数
+program.parse(process.argv)
