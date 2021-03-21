8fc393d128b301c9a2ce5450631841e3f56c60aa
Johnson <zxmagichacker@gmail.com>
2021-03-21 23:06:34 +0800

    feat: 添加交互选项

--- utils/questionLists.js
+++ utils/questionLists.js
@@ -1,18 +1,41 @@
 /**
  * inquirer的询问列表
+ * 先选plugin
+ * 再选Linter
+ * 最后选择prettier
  */
-
-const linterList = [
+const techList = [
   {
     type: 'list',
-    message: 'Check the linter you needed',
+    message: 'Pick the eslint plugin you needed:',
     name: 'key',
     choices: [
       {
-        name: 'Vue:linter'
+        name: 'eslint-plugin-vue'
       },
       {
-        name: 'React:linter'
+        name: 'eslint-plugin-react'
+      }
+    ],
+    filter: function(val) {
+      return val.toLowerCase();
+    }
+  }
+]
+const linterList = [
+  {
+    type: 'list',
+    message: 'Pick the eslint config you needed:',
+    name: 'key',
+    choices: [
+      {
+        name: 'Airbnb config'
+      },
+      {
+        name: 'Standard config'
+      },
+      {
+        name: 'Alloy config'
       }
     ],
     filter: function(val) {
@@ -23,12 +46,13 @@ const linterList = [
 
 const formatterList = [
   {
-    type: 'list',
+    type: 'checkbox',
     message: 'Check the formatter you needed',
     name: 'key',
     choices: [
       {
-        name: 'Prettier'
+        name: 'Prettier',
+        checked: true
       }
     ],
     filter: function(val) {
@@ -37,6 +61,7 @@ const formatterList = [
   }
 ]
 module.exports = {
+  techList,
   linterList,
   formatterList
 }
