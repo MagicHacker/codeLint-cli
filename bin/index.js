7fa4810b0f514680d4d3f37bd40575b504009f8f
Johnson <zxmagichacker@gmail.com>
2021-03-24 22:22:53 +0800

    feat: 监听未定义的指令

--- bin/index.js
+++ bin/index.js
@@ -6,18 +6,12 @@ const chalk = require('chalk')
 const ora = require('ora')
 const { inQInit } = require('../lib/inquirer')
 const { downInit } = require('../lib/download')
+
 program
   // 定义当前版本
   .version(version, '-v, --version', 'output the version number')
   // 定义用法
   .usage('<command>')
-
-program.on('--help', () => {
-  cFontsInit()
-  console.log()
-  console.log(`Run ${chalk.cyan('codeLint-cli <command> --help')} for detailed usage of given command.`)
-  console.log()
-})
 
 program
   // 定义初始化指令
@@ -31,5 +25,20 @@ program
     console.log('xx', linter, formatter)
     // spinner.fail()
   })
+  // 监听help
+  program.on('--help', () => {
+    cFontsInit()
+    console.log()
+    console.log(`Run ${chalk.cyan('codeLint-cli <command> --help')} for detailed usage of given command.`)
+    console.log()
+  })
+  
+  // 监听未定义的命令
+  program.on('command:*', function () {
+    console.log(`${chalk.red(`Invalid command: ${program.args.join(' ')}`)}`)
+    console.log(`${chalk.green('See --help for a list of available commands.')}`)
+    process.exit(1)
+  })
+
 // 解析参数
 program.parse(process.argv)
