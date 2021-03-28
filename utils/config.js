16116168196ff60892ac6fe0c7964d956b866e8e
Johnson <zxmagichacker@gmail.com>
2021-03-28 19:18:54 +0800

    feat: 添加配置内容

---
+++ utils/config.js
@@ -0,0 +1,21 @@
+/**
+ * 配置项
+ */
+const config = {
+  'alloy/vue': {
+    name: 'eslint-plugin-vue',
+    url: 'github'
+  },
+  'alloy/react': {
+    name: 'eslint-plugin-react',
+    url: 'MagicHacker/codeLintTpl#master'
+  },
+  'prettier': {
+    name: 'Prettier',
+    url: 'Prettier'
+  }
+}
+
+module.exports = {
+  config
+}
